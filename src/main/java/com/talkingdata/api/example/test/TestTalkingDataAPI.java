2fe602140de5e7683b4566a431068e0b1d10d81a
xiaolong.chen <xiaolong.chen@tendcloud.com>
2015-05-18 15:08:07 +0800

    修改查询地址，增加新的示例

--- src/main/java/com/talkingdata/api/example/test/TestTalkingDataAPI.java
+++ src/main/java/com/talkingdata/api/example/test/TestTalkingDataAPI.java
@@ -10,17 +10,23 @@ import java.io.IOException;
  */
 public class TestTalkingDataAPI {
     public static void main(String[] args) throws IOException {
-        //api url
-        String api_url = "https://www.talkingdata.com/openapi-analytics/olap/data.json/query";
+        //查询一段时间范围内，每日的新增用户
+        String api_url = "https://api.talkingdata.com/metrics/app/v1";
         JSONObject filter = new JSONObject();
-        filter.put("start","2015-04-01");
-        filter.put("end","2015-04-07");
-        filter.put("platformid_list",new int[]{1});
+        filter.put("start", "2015-04-01");
+        filter.put("end", "2015-04-07");
+        filter.put("platformid_list", new int[]{1});
         JSONObject params = new JSONObject();
-        params.put("filter",filter);
-        params.put("metrics",new String[]{"newuser"});
-        params.put("groupby","day");
-        params.put("accesskey","fb46c6980e294be483faf9dc2a12be35");
+        params.put("filter", filter);
+        params.put("metrics", new String[]{"newuser"});
+        params.put("groupby", "daily");
+        params.put("accesskey", "fb46*****e294be483fa*****a12be35");//accesskey should be replaced
         System.out.println(TalkingDataHttpClient.doPost(api_url, params));
+
+        //查询 version list，如果需要过滤平台可添加相应filter
+        String query_url = "https://api.talkingdata.com/metrics/app/v1/versionlist";
+        params = new JSONObject();
+        params.put("accesskey", "fb46*****e294be483fa*****a12be35");//accesskey should be replaced
+        System.out.println(TalkingDataHttpClient.doPost(query_url, params));
     }
 }
